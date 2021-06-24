5d446b3b4555469279bc903f44a4cfd057648f4c
biniam <biniesh87@gmail.com>
2021-06-24 13:32:11 -0700

    check lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times.
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	if ((c >= 97) && (c <= 122))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
