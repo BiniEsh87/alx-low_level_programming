6647c6e8b8a3ae808006b1386660e4d9c4737750
biniam <biniesh87@gmail.com>
2021-06-28 12:49:41 -0700

    fix

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,13 +8,16 @@
 
 void _puts(char *str)
 {
-int s = 0;
+	int count = 0;
 
-while (str[s] != '\0')
-{
-_putchar(str[s]);
-s++;
-}
-
-_putchar('\n');
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+		_putchar('\n');
+		break;
+		}
+		_putchar(str[count]);
+		count++;
+	}
 }
