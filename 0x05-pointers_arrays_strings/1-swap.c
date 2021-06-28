df53b585e1a33ac3244b8116a092daa8a6da5e81
biniam <biniesh87@gmail.com>
2021-06-28 11:31:32 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *reset_to_98 - it reset numbers to 98
+ *@n: an integer value,
+ *Return: returns nothing in void cases
+ */
+
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
+
