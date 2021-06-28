f10e1542d4a7623bbe2f0e1b969e2b666816577c
biniam <biniesh87@gmail.com>
2021-06-28 12:40:48 -0700

    fix

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *_puts - print a string
+ *
+ *Return: returns void
+ */
+
+void _puts(char *str)
+
+	int s = 0;
+
+	while (str[s] != '\0')
+	{
+	_putchar(str[1])
+	s++;
+	}
+	_putchar('\n')
+}
+
